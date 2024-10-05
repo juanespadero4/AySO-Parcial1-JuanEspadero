0f7546936c34e0fb0efa42bf025388ee17c0e93d
juanespadero4 <juancho1770@hotmail.com>
2024-10-05 21:20:38 +0000

    Ejercicio A y B

---
+++ puntob.sh
@@ -0,0 +1,30 @@
+33  sudo fdisk /dev/sdc
+   34  history
+   35  lsblk
+   36  sudo mkfs.ext4 /dev/sdc1
+   37  sudo mkfs.ext4 /dev/sdc2
+   38  sudo mkfs.ext4 /dev/sdc3
+   39  sudo mkfs.ext4 /dev/sdc4
+   40  cd..
+   41  cd ..
+   42  ll
+   43  cd parcial1
+   44  sudo mkdir -p Examenes-UTN/{alumno_{1..3}/parcial_{1..3},profesores}
+   45  ll
+   46  ll /mnt/
+   47  $ sudo mkdir /mnt/sdc1
+   48  $ sudo mkdir /mnt/sdc1/
+   49  $sudo mkdir /mnt/sdc1/
+   50  $sudo mkdir /mnt/sdc1
+   51  $ sudo mkdir /mnt/sdc1
+   52  sudo mkdir /mnt/sdc1
+   53  sudo mount /dev/sdc1
+   54  sudo mount /dev/sdc1 /mnt/sdc1
+   55  sudo mkdir /mnt/sdc2
+   56  sudo mount /dev/sdc2 /mnt/sdc2
+   57  sudo mkdir /mnt/sdc3
+   58  sudo mount /dev/sdc3 /mnt/sdc3
+   59  sudo mkdir /mnt/sdc4
+   60  sudo mount /dev/sdc4 /mnt/sdc4
+   61  ll /mnt/
+

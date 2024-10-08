e3d0468d36ac0f426026f8977bad0b3c187b6917
juanespadero4 <juancho1770@hotmail.com>
2024-10-08 04:03:03 +0000

    correccion ejercicio F

--- puntof.sh
+++ puntof.sh
@@ -1,11 +1,8 @@
- 137  curl -s ifconfig.me > filtro_avanzado.txt
-  138  whoami >> filtro_avanzado.txt
-  139  sudo cat /etc/shadow |grep vagrant |awk -F ':' '{print $2}' >> filtro_avanzado.txt
-  140  cat filtro_avanzado.txt
-  141  notepad filtro_avanzado.txt
-  142  rm Filtro_Avanzado.txt
-  143  ll
-  144  rm filtroAvanzado.txt
-  145  ll
-  146  nano filtro_avanzado.txt
-  147  cat filtro_avanzado.txt
+179  cat filtro_avanzado.txt
+180  echo "Mi IP publica es : ${curl -s ifconfig.me}" > filtro_avanzado.txt
+181  echo "Mi IP publica es : $(curl -s ifconfig.me)" > filtro_avanzado.txt
+182  echo "Mi usuario es : $(whoami)" >> filtro_avanzado.txt
+183  echo "El hash de mi usuario es : $(sudo cat /etc/shadow | grep vagrant | awk -F ':' '{print $2}')" >> filtro_avanzado.txt
+184  cat filtro_avanzado.txt
+185  history
+
